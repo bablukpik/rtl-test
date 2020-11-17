5ae4f88aa509dd22587fb9c710fb4298290b37c7
Bablu Ahmed <bablukpik@gmail.com>
2020-11-18 01:03:27 +0600

    mock test done

---
+++ src/AxiosMock.spec.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { render, cleanup, waitForElement } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import axios from "axios";
+import Fetch from "./AxiosMock";
+
+afterEach(cleanup);
+
+// Mock axios another way
+jest.mock('axios');
+
+it("fetches and displays data", async () => {
+    // We'll be explicit about what data Axios is to return when `get` is called.
+    axios.get.mockResolvedValueOnce({ data: { greeting: "hello there" } });
+
+    // Let's render our Fetch component, passing it the url prop and destructuring
+    // the `getByTestId` function so we can find individual elements.
+    const url = "/greeting";
+    const { getByTestId } = render(<Fetch url={url} />);
+
+    // On first render, we expect the "loading" span to be displayed
+    expect(getByTestId("loading")).toHaveTextContent("Loading data...");
+
+    // Because the useAxios call (useEffect) happens after initial render
+    // We need to handle the async nature of an AJAX call by waiting for the
+    // element to be rendered.
+    const resolvedSpan = await waitForElement(() => getByTestId("resolved"));
+
+    // Now with the resolvedSpan in hand, we can ensure it has the correct content
+    expect(resolvedSpan).toHaveTextContent("hello there");
+    // Let's also make sure our Axios mock was called the way we expect
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(url);
+});
